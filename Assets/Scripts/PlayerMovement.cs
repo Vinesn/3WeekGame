44ad7e28744ba9b9b382de51a1dd8419bdc8ada9
Vinesn <Muffinowaty739@gmail.com>
2024-12-17 23:29:55 +0100

    BackItUp

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,13 +10,23 @@ public class PlayerMovement : MonoBehaviour
     bool jumpInput;
 
     [SerializeField] float moveSpeed = 4f;
-    [SerializeField] float baseJumpForce = 3f;
+    [SerializeField] float jumpForce = 3f;
+    [SerializeField] float jumpTime;
+    [SerializeField] float coyoteTime = 0.2f;
+    
+    float jumpTimeCounter;
+    float coyoteTimeCounter;
+
+    [SerializeField] float fallMultiplier;
+    [SerializeField] float gravityPullMultiplier;
 
     Rigidbody2D rb;
     [SerializeField] Transform groundCheck;
+    [SerializeField] float groundCheckRadius = 0.2f;
     [SerializeField] LayerMask groundLayer;
 
     bool isGrounded;
+    bool isJumpButtonReleased;
 
     private void Awake()
     {
@@ -30,18 +41,18 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        Debug.Log(coyoteTimeCounter);
+
         isGrounded = IsGrounded();
         CheckInput();
+        CoyoteTimeHandler();
+        HandleJump();
     }
 
     private void FixedUpdate()
     {
         ApplyMovement();
-
-        if (jumpInput && isGrounded)
-        {
-            Jump();
-        }
+        AdjustGravity();
     }
 
     void CheckInput()
@@ -52,7 +63,15 @@ public class PlayerMovement : MonoBehaviour
 
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+        return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer); //Naprawiæ Hitbox, dzia³a tylko na œrodku postaci nie po rogach.
+    }
+    private void OnDrawGizmos()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
     }
 
     private void ApplyMovement()
@@ -60,8 +79,55 @@ public class PlayerMovement : MonoBehaviour
         rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, rb.linearVelocity.y);
     }
 
+    private void AdjustGravity()
+    {
+        if (rb.linearVelocity.y > 0)
+        {
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+        }
+        if (rb.linearVelocity.y < 0)
+        {
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (gravityPullMultiplier - 1) * Time.fixedDeltaTime;
+        }
+    }
+
+    void HandleJump()
+    {
+        if (jumpInput && coyoteTimeCounter > 0f && isJumpButtonReleased)
+        {
+            jumpTimeCounter = jumpTime;
+            Jump();
+            isJumpButtonReleased = false;
+        }
+
+        if (rb.linearVelocity.y > 0 && jumpInput)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                Jump();
+                jumpTimeCounter -= Time.deltaTime;
+            }
+        }
+
+        if (!jumpInput)
+        {
+            isJumpButtonReleased = true;
+        }
+    }
+    void CoyoteTimeHandler()
+    {
+        if (isGrounded)
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else 
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+    }
+
     private void Jump()
     {
-        rb.AddForce(Vector2.up * baseJumpForce, ForceMode2D.Impulse);
+        rb.linearVelocity = Vector2.up * jumpForce;
     }
 }
